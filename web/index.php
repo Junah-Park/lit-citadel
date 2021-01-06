c3f2c558414588df92383073a4c793fa5476ca41
Junah Park <junahp@hotmail.com>
2021-01-05 19:49:50 -0800

    initialize through index.php

--- web/index.php
+++ web/index.php
@@ -1,10 +1,12 @@
 <?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+  $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
+  $server = $url["host"];
+  $username = $url["user"];
+  $password = $url["pass"];
+  $db = substr($url["path"], 1);
 
-$conn = new mysqli($server, $username, $password, $db);
+  $conn = new mysqli($server, $username, $password, $db);
+  $conn->query("CREATE TABLE `unityaccess`.`example` ( `id` INT(10) NOT NULL AUTO_INCREMENT , `username` VARCHAR(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , `hash` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , `salt` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , `score` INT(10) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB; ");
+  echo "Success";
 ?>
--- web/initialize.php
+++ web/initialize.php
@@ -8,4 +8,5 @@
 
 	$conn = new mysqli($server, $username, $password, $db);
 	$conn->query("CREATE TABLE `unityaccess`.`example` ( `id` INT(10) NOT NULL AUTO_INCREMENT , `username` VARCHAR(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , `hash` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , `salt` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , `score` INT(10) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB; ");
+	echo "Success";
 ?>

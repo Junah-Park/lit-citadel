7187a5bf9bbedde29d97e952622f9fc72388d35f
Junah Park <junahp@hotmail.com>
2021-01-05 20:48:46 -0800

    modified create table

--- web/index.php
+++ web/index.php
@@ -6,7 +6,17 @@
   $password = $url["pass"];
   $db = substr($url["path"], 1);
 
-  $conn = new mysqli($server, $username, $password, $db); =
-  $conn->query("CREATE TABLE `users` ( `id` INT(10) NOT NULL AUTO_INCREMENT , `username` VARCHAR(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , `hash` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , `salt` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , `score` INT(10) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB; ");
-  echo $conn->query("SELECT username FROM users");
+// sql to create table
+$sql = "CREATE TABLE users ( id INT(10) NOT NULL AUTO_INCREMENT , 
+  username VARCHAR(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , 
+  hash VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , 
+  salt VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , 
+  score INT(10) NOT NULL , PRIMARY KEY (`id`))";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Table users created successfully";
+} else {
+  echo "Error creating table: " . $conn->error;
+}
+
 ?>

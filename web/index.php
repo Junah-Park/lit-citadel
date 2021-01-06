0dc1519c5a1377bb8b8a1c5049728e17c8627c04
Junah Park <junahp@hotmail.com>
2021-01-05 20:52:57 -0800

    modified SQL FOR create table

--- web/index.php
+++ web/index.php
@@ -7,11 +7,13 @@
   $db = substr($url["path"], 1);
 
 // sql to create table
-$sql = "CREATE TABLE users ( id INT(10) NOT NULL AUTO_INCREMENT , 
-  username VARCHAR(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , 
-  hash VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , 
-  salt VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , 
-  score INT(10) NOT NULL , PRIMARY KEY (`id`))";
+$sql = "CREATE TABLE users ( 
+  id INT(10) AUTO_INCREMENT PRIMARY KEY, 
+  username VARCHAR(16) NOT NULL, 
+  hash VARCHAR(100) NOT NULL, 
+  salt VARCHAR(50) NOT NULL, 
+  score INT(10) NOT NULL
+  )";
 
 if ($conn->query($sql) === TRUE) {
   echo "Table users created successfully";

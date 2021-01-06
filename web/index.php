46612347381b4f3e3a631674ae7bcd9986b6e9f1
Junah Park <junahp@hotmail.com>
2021-01-05 20:30:17 -0800

    test new query for index

--- web/index.php
+++ web/index.php
@@ -6,7 +6,7 @@
   $password = $url["pass"];
   $db = substr($url["path"], 1);
 
-  $conn = new mysqli($server, $username, $password, $db);
-  $conn->query("CREATE TABLE `unityaccess`.`example` ( `id` INT(10) NOT NULL AUTO_INCREMENT , `username` VARCHAR(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , `hash` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , `salt` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , `score` INT(10) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB; ");
-  echo "Success";
+  $conn = new mysqli($server, $username, $password, $db); =
+  $conn->query("CREATE TABLE `users` ( `id` INT(10) NOT NULL AUTO_INCREMENT , `username` VARCHAR(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , `hash` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , `salt` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , `score` INT(10) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB; ");
+  echo $conn->query("SELECT username FROM users");
 ?>

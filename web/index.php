88ec07c3f808575bc112909b0945fde7bfcb988a
Junah Park <junahp@hotmail.com>
2021-01-05 20:55:39 -0800

    modified SQL FOR create table added conn

--- web/index.php
+++ web/index.php
@@ -6,19 +6,22 @@
   $password = $url["pass"];
   $db = substr($url["path"], 1);
 
+  $conn = new mysqli($server, $username, $password, $db); 
 // sql to create table
-$sql = "CREATE TABLE users ( 
-  id INT(10) AUTO_INCREMENT PRIMARY KEY, 
-  username VARCHAR(16) NOT NULL, 
-  hash VARCHAR(100) NOT NULL, 
-  salt VARCHAR(50) NOT NULL, 
-  score INT(10) NOT NULL
-  )";
+  $sql = "CREATE TABLE users ( 
+    id INT(10) AUTO_INCREMENT PRIMARY KEY, 
+    username VARCHAR(16) NOT NULL, 
+    hash VARCHAR(100) NOT NULL, 
+    salt VARCHAR(50) NOT NULL, 
+    score INT(10) NOT NULL
+    )";
 
-if ($conn->query($sql) === TRUE) {
-  echo "Table users created successfully";
-} else {
-  echo "Error creating table: " . $conn->error;
-}
+  echo "sql valid"
+
+  if ($conn->query($sql) === TRUE) {
+    echo "Table users created successfully";
+  } else {
+    echo "Error creating table: " . $conn->error;
+  }
 
 ?>
